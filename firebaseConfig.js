fbe607b72782b1fe904c8e3098a6e559bfa10c53
Jaystorm56 <olamike83@yahoo.com>
2025-03-07 03:26:05 -0800

    secured firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBuB7vC5dE8-RCS9ywrcgmRrpB5ztJWvAc",
-  authDomain: "meezak-task-manager.firebaseapp.com",
-  projectId: "meezak-task-manager",
-  storageBucket: "meezak-task-manager.firebasestorage.app",
-  messagingSenderId: "682625141511",
-  appId: "1:682625141511:web:8fb48f744e501b7ed513a2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
