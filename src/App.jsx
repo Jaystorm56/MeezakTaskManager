86ad2a7edce28455541b9afd5783bd33f840f0c2
graceayomi <oayomideg@gmail.com>
2025-03-16 18:00:59 +0100

    Working Admin  sign-up/login features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import AdminSignIn from './pages/AdminSignIn';
+import AdminSignUp from './pages/AdminSignUp';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/adminsignin" element={<AdminSignIn />} />
+        <Route path="/adminsignup" element={<AdminSignUp />} />
         <Route
           path="/dashboard"
           element={
@@ -18,7 +22,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<AdminSignUp />} />
       </Routes>
     </Router>
   );
